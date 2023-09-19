fc921e1ee79712515002cf15d3b2e00bd894c1b8
alpah <dev@alpah.ru>
2023-09-19 18:09:30 +0200

    added close button

--- src/HtmlBasedDialog.vue
+++ src/HtmlBasedDialog.vue
@@ -1,18 +1,19 @@
 <template>
-    <dialog ref="dialogRef">
-        <h3>Dialog opened</h3>
-        <p>Try to inspect element below with vue-devtools inspector:</p>
-        <DialogChild />
-    </dialog>
+  <dialog ref="dialogRef">
+    <h3>Dialog opened</h3>
+    <p>Try to inspect element below with vue-devtools inspector:</p>
+    <DialogChild />
+    <button @click="$emit('close')">Close</button>
+  </dialog>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
-import DialogChild from './DialogChild';
+import { onMounted, ref } from "vue";
+import DialogChild from "./DialogChild";
 
 const dialogRef = ref();
 
 onMounted(() => {
-    dialogRef.value.showModal();
-})
+  dialogRef.value.showModal();
+});
 </script>
